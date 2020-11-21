db25c45d84c52ef0a509382b1de1416f485dff8f
crystalfn <969274162@qq.com>
2020-11-21 23:28:26 +0800

    [fangni] feat: implement p2s2

---
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTestZero.java
@@ -0,0 +1,28 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest(properties = { "levelNumber=0" })
+class LevelControllerTestZero {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void should_return_basic_when_level_number_is_zero() throws Exception {
+        mockMvc.perform(get("/level"))
+                .andExpect(jsonPath("$", is("basic")));
+    }
+}
