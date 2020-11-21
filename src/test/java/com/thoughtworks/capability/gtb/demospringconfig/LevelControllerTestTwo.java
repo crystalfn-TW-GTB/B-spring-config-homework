f4900d055f7e3c442f6232e39b33a46bdfc666c0
crystalfn <969274162@qq.com>
2020-11-21 23:16:03 +0800

    [fangni] feat: implement p2s1

---
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTestTwo.java
@@ -0,0 +1,30 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@ActiveProfiles("two")
+class LevelControllerTestTwo {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void should_return_advanced_when_level_number_is_two() throws Exception {
+        mockMvc.perform(get("/level"))
+                .andExpect(jsonPath("$", is("advanced")));
+    }
+}
